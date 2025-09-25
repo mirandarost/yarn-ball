263f9439fea77f4d17a866b60b5ab69b7402949e
Miranda Rost <miranda.rost@gmail.com>
2025-09-25 11:34:17 +0200

    cleaning up api variables

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,14 +1,17 @@
 'use server';
 
 import { getParsedPattern } from "./parse";
+import '@/app/envConfig.ts';
 
 
 export async function getPattern(id: string) {
     const patternUrl = `https://api.ravelry.com/patterns/${id}.json`
     // const url = 'https://api.ravelry.com/patterns/wool--honey.json'
 
-    const username = 'read-891e993c0dee11e1f77e823b9533f28d';
-    const password = 'ctFeFcJXL2X06TObIch1hgTDBOuTlzp+6lVlGW1Z';
+    const username = process.env.API_USERNAME;
+    const password = process.env.API_PASSWORD;
+
+    console.log(username, password);
 
     const headers = new Headers();
     headers.set('Authorization', 'Basic ' + Buffer.from(username + ":" + password).toString('base64'));
@@ -20,7 +23,7 @@ export async function getPattern(id: string) {
         return(parsedPattern);
 
     } catch (error) {
-        console.error("Pattern id not found");
+        console.error(error);
         return null;
     }
     
